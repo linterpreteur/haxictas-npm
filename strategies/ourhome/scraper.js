f7aca1ee0da28120fed1ebc58b7cac85781f8f77
Wooseop Kim <wooseop.kim.dev@gmail.com>
2019-12-24 23:07:30 +0900

    Allow unsafe HTTPS connection on OURHOME pages

--- strategies/ourhome/scraper.js
+++ strategies/ourhome/scraper.js
@@ -8,18 +8,26 @@ module.exports.menus = (date, callback) => {
     date = parseInt(date.getTime() / 1000, 10);
     
     const query = qs.stringify({'start_date2': date});
+    const httpsAgent = new https.Agent({
+        rejectUnauthorized: false
+    });
+    const opts = {httpsAgent: httpsAgent};
     httpHandler.get(
         `${url}?${query}`,
-        null,
+        opts,
         callback
     );
 };
 
 module.exports.cafeterias = (callback) => {
     const url = 'https://dorm.snu.ac.kr/dk_board/dk_dormitory/dorm_content.php?ht_id=snu2_1_1';
+    const httpsAgent = new https.Agent({
+        rejectUnauthorized: false
+    });
+    const opts = {httpsAgent: httpsAgent};
     httpHandler.get(
         url,
-        null,
+        opts,
         callback
     );
 };
