eb26140f42dd50c27026c81bc1c6939751c8cd90
Wooseop Kim <wooseop.kim.dev@gmail.com>
2019-12-25 13:51:04 +0900

    Add module ID to example outputs

--- example/index.js
+++ example/index.js
@@ -1,13 +1,13 @@
 const haxictas = require('..')('./example/third_party');
 
-const cafeteriasCallback = (info, err) => {
+const cafeteriasCallback = id => (info, err) => {
     if (err) {
         return console.error(err);
     }
-    console.log(JSON.stringify(info, null, 2));
+    console.log(id, JSON.stringify(info, null, 2));
 };
 
-const menusCallback = (result, err) => {
+const menusCallback = id => (result, err) => {
     if (err) {
         return console.error(err);
     }
@@ -15,16 +15,16 @@ const menusCallback = (result, err) => {
     date = new Date(date);
     date.setDate(date.getDate() + (day - date.getDay()) % 7);
     data.date = date.valueOf();
-    console.log(date.toString(), day, JSON.stringify(data, null, 2));
+    console.log(id, date.toString(), day, JSON.stringify(data, null, 2));
 };
 
-haxictas.snuco.cafeterias(cafeteriasCallback);
-haxictas.ourhome.cafeterias(cafeteriasCallback);
+haxictas.snuco.cafeterias(cafeteriasCallback('snuco'));
+haxictas.ourhome.cafeterias(cafeteriasCallback('ourhome'));
 
 const date = new Date();
-haxictas.snuco.menus(date, menusCallback);
-haxictas.ourhome.menus(date, menusCallback);
-haxictas.mini.menus(date, menusCallback);
+haxictas.snuco.menus(date, menusCallback('snuco'));
+haxictas.ourhome.menus(date, menusCallback('ourhome'));
+haxictas.mini.menus(date, menusCallback('mini'));
 
-haxictas.new_cafeteria.menus(date, menusCallback);
-haxictas.new_cafeteria.cafeterias(cafeteriasCallback);
+haxictas.new_cafeteria.menus(date, menusCallback('new_cafeteria'));
+haxictas.new_cafeteria.cafeterias(cafeteriasCallback('new_cafeteria'));
